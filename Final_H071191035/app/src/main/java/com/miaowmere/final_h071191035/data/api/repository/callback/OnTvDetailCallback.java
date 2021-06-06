f660a4b9820fbadc720d12be9b44762cece500e6
miaowmere <54744527+trueazp@users.noreply.github.com>
2021-06-06 10:10:24 +0800

    Delete Final_H071191035 directory

--- Final_H071191035/app/src/main/java/com/miaowmere/final_h071191035/data/api/repository/callback/OnTvDetailCallback.java
+++
@@ -1,9 +0,0 @@
-package com.miaowmere.final_h071191035.data.api.repository.callback;
-
-import com.miaowmere.final_h071191035.data.models.TvShow;
-
-public interface OnTvDetailCallback {
-    void onSuccess(TvShow tvShow, String message);
-
-    void onFailure(String message);
-}
