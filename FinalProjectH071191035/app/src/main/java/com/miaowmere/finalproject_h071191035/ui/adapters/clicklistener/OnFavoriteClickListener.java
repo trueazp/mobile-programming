8d1c3aea4482b07900c53e5d56418cbbfe54b1c3
miaowmerE <54744527+trueazp@users.noreply.github.com>
2021-06-06 17:43:33 +0800

    delete

--- FinalProjectH071191035/app/src/main/java/com/miaowmere/finalproject_h071191035/ui/adapters/clicklistener/OnFavoriteClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.miaowmere.finalproject_h071191035.ui.adapters.clicklistener;
-
-import com.miaowmere.finalproject_h071191035.data.models.Favorite;
-
-public interface OnFavoriteClickListener {
-    void onClick(Favorite favorite);
-}
