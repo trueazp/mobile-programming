bb813da487dae9939d8a5041502257a4e3be5d7a
miaowmerE <54744527+trueazp@users.noreply.github.com>
2021-06-07 08:19:11 +0800

    plugin update, add bottom nav logic, add splash screen

--- FinalProjectH071191035/app/src/main/java/com/miaowmere/finalproject_h071191035/activities/SplashScreenActivity.java
+++ FinalProjectH071191035/app/src/main/java/com/miaowmere/finalproject_h071191035/activities/SplashScreenActivity.java
@@ -2,15 +2,28 @@ package com.miaowmere.finalproject_h071191035.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import com.miaowmere.finalproject_h071191035.R;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(mainIntent);
+            }
+        }, 3000);
     }
 }
